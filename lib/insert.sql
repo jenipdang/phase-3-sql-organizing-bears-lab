1dfcd5aae73ef230b9ff851325581a8a864ba3e7
Jeni Dang <jenipdang@yahoo.com>
2022-03-02 19:57:02 -0800

    commit tree 0a6d5f8c64bbf25e9f96ba3eeadb0362ea9fbe9d

---
+++ lib/insert.sql
@@ -0,0 +1,8 @@
+INSERT INTO bears VALUES (1, "Mr.Chocolate", 20, "M", "Brown", "Chill", 1);
+INSERT INTO bears VALUES (2, "Rowdy", 10, "M", "Red", "Hyper", 1);
+INSERT INTO bears VALUES (3, "Tabitha", 5, "F", "Brown", "Chill", 1);
+INSERT INTO bears VALUES (4, "Sergean Brown", 30, "M", "Dark", "Grumpy", 0);
+INSERT INTO bears VALUES (5, "Melissa", 10, "F", "Yellow", "Wild", 1);
+INSERT INTO bears VALUES (6, "Grinch", 40, "M", "Green", "Naughty", 1);
+INSERT INTO bears VALUES (7, "Wendy", 15, "F", "Yellow", "Chill", 0);
+INSERT INTO bears VALUES (8, null, 10, "M", "Brown", "Chatty", 1);
